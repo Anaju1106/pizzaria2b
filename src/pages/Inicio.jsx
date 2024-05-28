3bce60ad997276556025cddcc1254ae2ddf381c0
Anaju1106 <anajumoura1106@gmail.com>
2024-05-28 09:15:15 -0300

    primeiro commit

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,15 @@
+import {Link, useNavigate} from 'react-router-dom' 
+import logo from '../assets/react.svg'
+import '../App.css'
+
+const Inicio = () => {
+
+    return (
+        <div>
+           <p>Outra tela Inicial</p>
+           <img className='img-fluid' sr{logo}
+           alt='logo'> </img>
+        </div>
+    )
+}
+export default Inicio
